153d94b8788058a4c4e24197ee78c3e5b5c9a00e
Gabriel <gabriel@example.com>
2026-05-31 20:35:41 -0300

    Fix Expo asset resolution

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'expo-asset': path.resolve(__dirname, 'node_modules/expo-asset'),
+};
+
+module.exports = config;
